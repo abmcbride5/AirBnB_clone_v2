5fea38729e9da27a4beb2607c76c93cd01c07d3d
Amber <abmcbride5@gmail.com>
2019-08-14 20:41:36 +0000

    updating

--- models/state.py
+++ models/state.py
@@ -8,4 +8,24 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-ophan")
+    name = Column(String(128), nullable=False)
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ returns list of City instances with state_id matching the
+            current Stated id
+            """
+            objects = models.storage.all(City)
+            a_list = []
+            for obj in objects.values():
+                if obj.state_id == self.id:
+                    a_list.append(obj)
+            return a_list
