968393fd131b6c27ebb37cce14b56aa8f464147d
Amber <abmcbride5@gmail.com>
2019-08-14 22:59:56 +0000

    updating

--- models/state.py
+++ models/state.py
@@ -9,16 +9,16 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
-        name: input name
+    name: input name
     """
     __tablename__ = "states"
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="state",
-                              cascade="all, delete-ophan")
+                              cascade="all, delete-orphan")
         name = Column(String(128), nullable=False)
 
     else:
@@ -26,8 +26,8 @@ class State(BaseModel):
 
         @property
         def cities(self):
-            """ returns list of City instances with state_id matching the
-            current Stated id
+            """returns list of City instances with state_id matching the
+            current State id
             """
             objects = models.storage.all(City)
             a_list = []
