5f5d9fe440fa1ce489713fb8433a7c340c4d0873
Amber <abmcbride5@gmail.com>
2019-08-14 20:33:40 +0000

    updating

--- models/city.py
+++ models/city.py
@@ -9,5 +9,11 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    state_id = Column(String(60), ForeignKey('states.id'),
+                      nullable=False)
+    name = Column(String(128), nullable=False)
+
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete-orphan")
