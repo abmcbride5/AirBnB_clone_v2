b5627f3c9065c52512e8b04b290b8acaf545755f
Amber <abmcbride5@gmail.com>
2019-08-15 07:11:42 +0000

    updating

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,7 +7,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 
@@ -16,8 +16,8 @@ class DBStorage:
     """This class stores instances in tables and
     converts tables to instances
     Attributes:
-    __engine: connection to mysql db
-    __session: sqlalchemy mysql session
+        __engine: connection to mysql db
+        __session: sqlalchemy mysql session
     """
     __engine = None
     __session = None
@@ -29,6 +29,7 @@ class DBStorage:
         dialect: mysql
         driver: mysqldb
         """
+        # Get environment variables and store them
         user = getenv('HBNB_MYSQL_USER')
         password = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
@@ -39,12 +40,12 @@ class DBStorage:
         self.__engine = create_engine(sql, pool_pre_ping=True)
 
         if getenv('HBNB_ENV') is not None and getenv('HBNB_ENV') == "test":
-            Base.metadata.drop_all(bind=self.__engine)
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """query on the current database
         Return:
-        returns a dictionary of object
+            returns a dictionary of object
         """
         a_d = {}
         if cls:
@@ -62,7 +63,7 @@ class DBStorage:
     def new(self, obj):
         """adds obj to __session
         Args:
-        obj: given object
+            obj: given object
         """
         if obj:
             self.__session.add(obj)
@@ -85,4 +86,4 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
