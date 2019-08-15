03ce5518b0e9acd7ab5c7de9780cce2f598dbd0c
Amber <abmcbride5@gmail.com>
2019-08-15 07:29:42 +0000

    updating

--- models/place.py
+++ models/place.py
@@ -2,6 +2,7 @@
 """This is the place class"""
 from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
