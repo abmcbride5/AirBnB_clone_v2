368d1d5d19d19b9c36ccc901d439c3477152f3c0
Amber <abmcbride5@gmail.com>
2019-08-15 07:46:49 +0000

    updating

--- models/review.py
+++ models/review.py
@@ -12,5 +12,8 @@ class Review(BaseModel):
         text: review description
     """
     __tablename__ = "reviews"
-    text = Column(String(60), ForeignKey('places.id'),
-                  nullable=False)
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey(places.id),
+                      nullable=False)
+    user_id = Column(Stirng(60), ForeignKey(users.id),
+                     nullable=False)
