b850a8a72d06ac99a47ed5d8fbf1319b6ff5d5ef
Amber <abmcbride5@gmail.com>
2019-08-15 07:06:00 +0000

    updating

--- models/city.py
+++ models/city.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """This is the class for City
     Attributes:
-    state_id: The state id
-    name: input name
+        state_id: The state id
+        name: input name
     """
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'),
