d8b32a76a6f7206059261fea9f45eb7b30107fcf
Amber <abmcbride5@gmail.com>
2019-08-15 00:54:08 +0000

    updating

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    __table__ = "users"
+    __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
