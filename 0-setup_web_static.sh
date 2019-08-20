6cb46e466cee121fe1e7a11ef91bed1976957e36
Amber <abmcbride5@gmail.com>
2019-08-20 05:16:49 +0000

    updating

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,12 @@
 # script that configures servers
 apt-get update
 dpkg -l | grep -qw nginx || apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p data/web_static/shared
 mkdir -p data/web_static/releases/test
-echo "hello" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+echo "hello" > ./data/web_static/releases/test/index.html
+ln -sf ./data/web_static/releases/test/ ./data/web_static/current
+chown -R ubuntu:ubuntu ./data/
 sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
