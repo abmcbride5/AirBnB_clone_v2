f4492646309eb5e81ec3265dba94ce4a632b4a81
Amber <abmcbride5@gmail.com>
2019-08-20 05:05:59 +0000

    updating

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p data/web_static/releases/test
 echo "hello" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
