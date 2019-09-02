996c2d0cfaf22362c5e6f41c56663db00f8f349a
Amber <abmcbride5@gmail.com>
2019-09-02 03:54:11 +0000

    adding 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" hello with flask"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cIsCool(text):
+    """ display text variable"""
+    return 'C %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIsCool(text="is cool"):
+    """ python is cool"""
+    return 'Python %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """ number route"""
+    return '%d is a number' % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
