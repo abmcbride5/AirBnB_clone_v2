6db8e3a80bb299603f1562e850f8f9a14af167cc
Amber <abmcbride5@gmail.com>
2019-09-05 06:59:20 +0000

    starting 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" fetch all states"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ list states in database"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """ close"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_states():
+    """ cities and states"""
+    states = storage.all("State")
+    cities = storage.all("City")
+    return render_template('7-states_list.html', states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """ close"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
